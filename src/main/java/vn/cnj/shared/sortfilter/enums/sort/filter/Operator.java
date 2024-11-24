2270bbb82e749b05d5dcaab8dc53b45221bbfe80
DatDT <thanhdat2599@gmail.com>
2024-11-24 10:21:40 +0700

    1.0.7

--- src/main/java/vn/cnj/shared/sortfilter/enums/sort/filter/Operator.java
+++ src/main/java/vn/cnj/shared/sortfilter/enums/sort/filter/Operator.java
@@ -18,6 +18,7 @@ public enum Operator {
             return cb.and(cb.equal(key, value), predicate);
         }
     },
+
     EQUAL_OR {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             Object value = request.getFieldType().parse(request.getValue().toString());
@@ -31,12 +32,7 @@ public enum Operator {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             Object value = request.getFieldType().parse(request.getValue().toString());
             String[] relations = request.getKey().split("\\.");
-            Path<?> path;
-            if (relations.length <= 1) {
-                path = root.get(request.getKey());
-            } else {
-                path = root.join(relations[0]).get(relations[1]);
-            }
+            Path<?> path = getPath(root, request, relations);
 
             if (path.getJavaType().equals(Integer.class)) {
                 return cb.or(cb.greaterThanOrEqualTo(path.as(Integer.class), (Integer) value), predicate);
@@ -60,6 +56,148 @@ public enum Operator {
         }
     },
 
+    LTE_OR {
+        public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
+            Object value = request.getFieldType().parse(request.getValue().toString());
+            String[] relations = request.getKey().split("\\.");
+            Path<?> path = getPath(root, request, relations);
+
+            if (path.getJavaType().equals(Integer.class)) {
+                return cb.or(cb.lessThanOrEqualTo(path.as(Integer.class), (Integer) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Long.class)) {
+                return cb.or(cb.lessThanOrEqualTo(path.as(Long.class), (Long) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Float.class)) {
+                return cb.or(cb.lessThanOrEqualTo(path.as(Float.class), (Float) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Double.class)) {
+                return cb.or(cb.lessThanOrEqualTo(path.as(Double.class), (Double) value), predicate);
+            }
+            if (path.getJavaType().equals(BigDecimal.class)) {
+                return cb.or(cb.lessThanOrEqualTo(path.as(BigDecimal.class), (BigDecimal) value), predicate);
+            }
+            throw new RuntimeException("Unsupported number type for GT operation");
+        }
+    },
+
+    GT {
+        public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
+            Object value = request.getFieldType().parse(request.getValue().toString());
+            String[] relations = request.getKey().split("\\.");
+            Path<?> path = getPath(root, request, relations);
+
+            if (path.getJavaType().equals(Integer.class)) {
+                return cb.and(cb.greaterThan(path.as(Integer.class), (Integer) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Long.class)) {
+                return cb.and(cb.greaterThan(path.as(Long.class), (Long) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Float.class)) {
+                return cb.and(cb.greaterThan(path.as(Float.class), (Float) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Double.class)) {
+                return cb.and(cb.greaterThan(path.as(Double.class), (Double) value), predicate);
+            }
+            if (path.getJavaType().equals(BigDecimal.class)) {
+                return cb.and(cb.greaterThan(path.as(BigDecimal.class), (BigDecimal) value), predicate);
+            }
+            throw new RuntimeException("Unsupported number type for GT operation");
+        }
+    },
+
+    GT_OR {
+        public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
+            Object value = request.getFieldType().parse(request.getValue().toString());
+            String[] relations = request.getKey().split("\\.");
+            Path<?> path = getPath(root, request, relations);
+
+            if (path.getJavaType().equals(Integer.class)) {
+                return cb.or(cb.greaterThan(path.as(Integer.class), (Integer) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Long.class)) {
+                return cb.or(cb.greaterThan(path.as(Long.class), (Long) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Float.class)) {
+                return cb.or(cb.greaterThan(path.as(Float.class), (Float) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Double.class)) {
+                return cb.or(cb.greaterThan(path.as(Double.class), (Double) value), predicate);
+            }
+            if (path.getJavaType().equals(BigDecimal.class)) {
+                return cb.or(cb.greaterThan(path.as(BigDecimal.class), (BigDecimal) value), predicate);
+            }
+            throw new RuntimeException("Unsupported number type for GT operation");
+        }
+    },
+
+
+    LT {
+        public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
+            Object value = request.getFieldType().parse(request.getValue().toString());
+            String[] relations = request.getKey().split("\\.");
+            Path<?> path = getPath(root, request, relations);
+
+            if (path.getJavaType().equals(Integer.class)) {
+                return cb.and(cb.lessThan(path.as(Integer.class), (Integer) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Long.class)) {
+                return cb.and(cb.lessThan(path.as(Long.class), (Long) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Float.class)) {
+                return cb.and(cb.lessThan(path.as(Float.class), (Float) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Double.class)) {
+                return cb.and(cb.lessThan(path.as(Double.class), (Double) value), predicate);
+            }
+            if (path.getJavaType().equals(BigDecimal.class)) {
+                return cb.and(cb.lessThan(path.as(BigDecimal.class), (BigDecimal) value), predicate);
+            }
+            throw new RuntimeException("Unsupported number type for GT operation");
+        }
+    },
+
+    LT_OR {
+        public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
+            Object value = request.getFieldType().parse(request.getValue().toString());
+            String[] relations = request.getKey().split("\\.");
+            Path<?> path = getPath(root, request, relations);
+
+            if (path.getJavaType().equals(Integer.class)) {
+                return cb.or(cb.lessThan(path.as(Integer.class), (Integer) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Long.class)) {
+                return cb.or(cb.lessThan(path.as(Long.class), (Long) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Float.class)) {
+                return cb.or(cb.lessThan(path.as(Float.class), (Float) value), predicate);
+            }
+
+            if (path.getJavaType().equals(Double.class)) {
+                return cb.or(cb.lessThan(path.as(Double.class), (Double) value), predicate);
+            }
+            if (path.getJavaType().equals(BigDecimal.class)) {
+                return cb.or(cb.lessThan(path.as(BigDecimal.class), (BigDecimal) value), predicate);
+            }
+            throw new RuntimeException("Unsupported number type for GT operation");
+        }
+    },
+
+
     NOT_EQUAL {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             Object value = request.getFieldType().parse(request.getValue().toString());
@@ -68,21 +206,21 @@ public enum Operator {
         }
     },
 
-    LIKE {
+    @SuppressWarnings("unchecked") LIKE {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             Expression<String> key = (Expression<String>) getExpressionWithJoining(root, request);
             return cb.and(cb.like(cb.upper(key), "%" + request.getValue().toString().toUpperCase() + "%"), predicate);
         }
     },
 
-    LIKE_OR {
+    @SuppressWarnings("unchecked") LIKE_OR {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             Expression<String> key = (Expression<String>) getExpressionWithJoining(root, request);
             return cb.or(predicate, cb.like(cb.upper(key), "%" + request.getValue().toString().toUpperCase() + "%"));
         }
     },
 
-    IN {
+    @SuppressWarnings("unchecked") IN {
         public <T> Predicate build(Root<T> root, CriteriaBuilder cb, FilterRequest request, Predicate predicate) {
             String[] relations = request.getKey().split("\\.");
             Expression<String> key = (Expression<String>) getExpressionWithJoining(root, request);
@@ -118,6 +256,16 @@ public enum Operator {
             return predicate;
         }
     };
+
+    private static <T> Path<?> getPath(Root<T> root, FilterRequest request, String[] relations) {
+        Path<?> path;
+        if (relations.length <= 1) {
+            path = root.get(request.getKey());
+        } else {
+            path = root.join(relations[0]).get(relations[1]);
+        }
+        return path;
+    }
 
     private static <T> Expression<?> getExpressionWithJoining(Root<T> root, FilterRequest request) {
         String[] relations = request.getKey().split("\\.");
